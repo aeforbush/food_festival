e27a286e0aa9701bd9302248741e6ea10df68886
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-23 11:02:20 -0700

    created end points for modularized code

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
-
 const path = require("path");
-const  webpack  = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
+const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // sets up the basic pieces to get up and running
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
   // taking bundled code and sending to dist/ best practice
   output: {
-    // resolve gets two arguments
-    path: path.join(__dirname, "dist"),
-    filename: "main.bundle.js",
+    filename: "[name].bundle.js",
+    path:__dirname + "dist",
   },
   // add plugins to direct webpack what to do
   plugins: [
@@ -20,8 +23,8 @@ module.exports = {
       jQuery: "jquery",
     }),
     new BundleAnalyzerPlugin({
-        analyzerMode: "static", // the report outputs to an HTML file in the dist folder  // can also set to disable to stop
-    })
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder  // can also set to disable to stop
+    }),
   ],
   // mode to run the webpack [default in production mode offers minify amplify] // webpack auto minifies code
   // development offers hot reloading and debugging
